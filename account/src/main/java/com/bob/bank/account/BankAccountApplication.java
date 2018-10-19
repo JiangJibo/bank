6b7a93e438e49c23c00e7da8ae9a96f07db28fff
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-10-19 12:13:48 +0800

    初始化bank-account

--- account/src/main/java/com/bob/bank/account/BankAccountApplication.java
+++ account/src/main/java/com/bob/bank/account/BankAccountApplication.java
@@ -1,18 +1,17 @@
-package com.bob.bank.user;
+package com.bob.bank.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.bootstrap.BootstrapConfiguration;
 
 /**
  * @author wb-jjb318191
- * @create 2018-10-19 11:40
+ * @create 2018-10-19 12:10
  */
 @SpringBootApplication
-public class BankUserApplication {
+public class BankAccountApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BankUserApplication.class, args);
+        SpringApplication.run(BankAccountApplication.class, args);
     }
 
 }
