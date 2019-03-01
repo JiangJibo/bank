327ed97d9f45d592949c03072f0375c3a132861f
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2019-03-01 17:07:59 +0800

    添加整合kafka模块

---
+++ kafka/src/main/java/com/bob/kafka/kafka/BankKafkaApplication.java
@@ -0,0 +1,17 @@
+package com.bob.kafka.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wb-jjb318191
+ * @create 2019-03-01 16:51
+ */
+@SpringBootApplication
+public class BankKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankKafkaApplication.class, args);
+    }
+
+}
