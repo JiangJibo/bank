8ec0e6f84f6566ad8aae1658d00098b219720802
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2019-04-17 16:00:22 +0800

    添加Elasticsearch模块

--- elasticsearch/src/main/java/com/bob/elasticsearch/controller/GoodsController.java
+++ elasticsearch/src/main/java/com/bob/elasticsearch/controller/GoodsController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
  * @author wb-jjb318191
  * @create 2019-04-17 15:35
